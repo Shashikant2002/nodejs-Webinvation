07a3e1fbeeb336cf074cea53a4ce11adb6b06a02
Shashikant <shashikantprajapti17@gmail.com>
2022-09-09 09:20:57 +0530

    web

--- server/app.js
+++ server/app.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const app = express();
-require("./db/connection");
+// require("./db/connection");
+// const cors = require("cors");
 const theamRoute = require("./routes/theamRoute");
-const cors = require("cors");
 const port = 4000;
 
 app.use(express.json);
-app.use(cors());
+// app.use(cors());
+
 app.use(theamRoute);
 
-app.use("/uploads", express.static("./uploads"));
+// app.use("/uploads", express.static("./uploads"));
 
 app.listen(port, () => {
   console.log("Server is Running on Port 4000");
