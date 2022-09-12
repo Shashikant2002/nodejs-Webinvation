ae4a0b0b0be118281b75ffbcad2550e6595fc567
Shashikant <shashikantprajapti17@gmail.com>
2022-09-12 20:51:58 +0530

    Filter Set

--- client/src/Components/Projects.js
+++ client/src/Components/Projects.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
-import AllCategory from "./AllCategory";
 const axios = require("axios");
 
 const Projects = () => {
   const [data, setData] = useState([]);
+  const [category, setcategory] = useState([]);
 
   const allTheam = async () => {
     const response = await axios.get("/findAllTheam", {
@@ -16,19 +16,29 @@ const Projects = () => {
     if (response.data.status === 401 || !response.data) {
       console.log("Error");
     } else {
-      console.log("Done");
       setData(response.data.theam);
+      setcategory(response.data.theam);
     }
   };
 
-  console.log(data);
   useEffect(() => {
     allTheam();
   }, []);
 
+  const filterCard = (cur = "All") => {
+    if (cur === "All") {
+      setData(category);
+    } else {
+      const updateList = category.filter((curEle) => {
+        return curEle.category === cur;
+      });
+      setData(updateList);
+    }
+  };
+
   const uniqueList = [
     ...new Set(
-      data.map((curEle) => {
+      category.map((curEle) => {
         return curEle.category;
       })
     ),
@@ -36,14 +46,34 @@ const Projects = () => {
 
   return (
     <>
-    <AllCategory category={data} />
+      <div className="Category">
+        <div className="container">
+          <button
+            onClick={() => filterCard()}
+            className="glButton"
+          >
+            All
+          </button>
+          {uniqueList.map((cur, index) => {
+            return (
+              <button
+                onClick={() => filterCard(cur)}
+                className="glButton"
+                key={index}
+              >
+                {cur}
+              </button>
+            );
+          })}
+        </div>
+      </div>
       <div className="projects">
         <div className="container flex wrap justContentCenter">
           {data?.map((cur, index) => (
             <div className="project" key={index}>
               <img src={`/uploads/${cur.image}`} alt="demo" />
               <h2>
-                <a href={cur.liveSite}>{cur?.title}</a>
+                <a href={cur.liveSite}>{cur.title}</a>
               </h2>
             </div>
           ))}
